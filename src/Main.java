dbf11e1811532fe6f938e9d4b5ad191898028725
”Camopogok” <zagrebin.dm@gmail.com>
2023-01-20 19:53:46 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class Main {
+    public Main () {
+        JFrame frame = new JFrame("Пробел");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(200,200);
+        frame.setLocation(400,300);
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+                    JOptionPane.showMessageDialog(null, "               Дмитрий");
+                }
+            }
+        });
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
